9776e4e8c6fa81525616b5bb7070d64e2e45c17e
Anantha Krishnaswamy <ananthakrishnaswamy@Ananthas-MacBook-Pro.local>
2026-02-13 15:37:57 +0530

    Add prisma generate on build

--- app/api/admin/vendors/route.ts
+++ app/api/admin/vendors/route.ts
@@ -31,7 +31,7 @@ export async function POST(req: NextRequest) {
       type,
       location,
       basePrice,
-      halls:
+      hall:
         type === "HALL"
           ? {
               create: {
